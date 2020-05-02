8319e3d2f5b4bf496045878076314c1519c66aa4
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2020-05-02 13:51:44 -0300

    att_01
    Inserindo cenários e fazendo a queda do Flappy Bird

---
+++ jogo.js
@@ -0,0 +1,100 @@
+console.log('[DevSoutinho] Flappy Bird');
+console.log('Inscreva-se no canal :D https://www.youtube.com/channel/UCzR2u5RWXWjUh7CwLSvbitA');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
+
+const planoDeFundo = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 275,
+    altura: 246,
+    x: 0,
+    y: canvas.height - 316,
+    desenha() {
+        contexto.fillStyle = '#70c5ce';
+        contexto.fillRect(0,0, canvas.width, canvas.height);
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            planoDeFundo.x, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+        );
+
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+        );
+    },
+};
+
+
+const chao = {
+    spriteX: 0,
+    spriteY: 610,
+    largura: 224,
+    altura: 112,
+    x: 0,
+    y: canvas.height - 112,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura,
+        );
+
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            (chao.x + chao.largura), chao.y,
+            chao.largura, chao.altura,
+        );
+    },
+};
+
+const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 34,
+    altura: 24,
+    x: 10,
+    y: 50,
+    gravidade: 0.25,
+    velocidade: 0,
+    atualiza() {
+        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
+        flappyBird.y = flappyBird.y + flappyBird.velocidade;
+    },
+    desenha()  {
+        contexto.drawImage(
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY, 
+            flappyBird.largura, flappyBird.altura, 
+            flappyBird.x, flappyBird.y, 
+            flappyBird.largura, flappyBird.altura, 
+        );
+    },
+};
+
+function loop() {
+    flappyBird.atualiza();
+    
+    planoDeFundo.desenha();
+    chao.desenha();
+    flappyBird.desenha();
+    
+    
+    requestAnimationFrame(loop);
+}
+
+loop();
